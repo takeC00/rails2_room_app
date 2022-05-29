d18ffe13c065f5c1c860f93389a75fa3a2021c0c
服部剛士 <take.614@icloud.com>
2022-05-29 13:57:53 +0900

    migration修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,7 @@ class Reservation < ApplicationRecord
   belongs_to :room
 
   def stay_day 
-    (end_day - start_day).to_i / 86400
+    (end_day - start_day).to_i / 86400 * person_number
   end
 
 end
